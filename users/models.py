830259ca9e794a3952ef5f7cc8b91e80b7cde963
R-droid101 <raggav.subramani@gmail.com>
2024-09-18 16:41:56 +0530

    feat: create and update profile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=300)
+    birthday = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -17,5 +17,24 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')  
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            username = user_form.cleaned_data.get('username')
+            messages.success(request, f'Profile updated successfully for {username}!')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+
+    return render(request, 'users/profile.html', context)  
 
